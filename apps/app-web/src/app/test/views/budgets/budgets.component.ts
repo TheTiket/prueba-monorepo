b0368f411b4c7836a20dee4bf058efd51dca56f9
NikolayCarrasco <nikolay.carrasco.v@gmail.com>
2022-09-26 17:11:02 -0300

    feat(app-web/budgets): view done

--- apps/app-web/src/app/test/views/budgets/budgets.component.ts
+++ apps/app-web/src/app/test/views/budgets/budgets.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budgets.component.scss'],
 })
 export class BudgetsComponent implements OnInit {
-  constructor() {}
+
+  public budgets;
+
+  constructor() {
+    this.budgets = [
+      {id: 1, name:'Juan'},
+      {id: 2, name:'Pedro'},
+      {id: 3, name:'Maria'},
+      {id: 4, name:'Diego'},
+      {id: 5, name:'Marta'},
+      {id: 6, name:'Camila'}
+    ]; 
+  }
 
   ngOnInit(): void {}
 }
