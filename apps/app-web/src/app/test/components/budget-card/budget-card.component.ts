e57f536db0e52a7609e02fc8cfb5fdc692ee09af
NikolayCarrasco <nikolay.carrasco.v@gmail.com>
2022-09-26 16:28:12 -0300

    feat(app-web): angular material module

---
+++ apps/app-web/src/app/test/components/budget-card/budget-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'prueba-monorepo-budget-card',
+  templateUrl: './budget-card.component.html',
+  styleUrls: ['./budget-card.component.scss'],
+})
+export class BudgetCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- apps/app-web/src/app/test/test.module.ts
+++ apps/app-web/src/app/test/test.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { TestRoutingModule } from './test-routing.module';
 import { BudgetsComponent } from './views/budgets/budgets.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BudgetCardComponent } from './components/budget-card/budget-card.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-  declarations: [BudgetsComponent],
-  imports: [CommonModule, TestRoutingModule],
+  declarations: [BudgetsComponent, NavbarComponent, BudgetCardComponent],
+  imports: [CommonModule, TestRoutingModule, MaterialModule],
 })
 export class TestModule {}
