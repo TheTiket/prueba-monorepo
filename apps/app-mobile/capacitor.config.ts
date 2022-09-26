f50c7e8ac36d242a50fe1d2de64eae0e7a29b130
Anibal <anibal78e@gmail.com>
2022-09-26 11:23:02 -0300

    feat(apps): mobile app

---
+++ apps/app-mobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-mobile',
+  webDir: '../../dist/apps/app-mobile',
+  bundledWebRuntime: false,
+};
+
+export default config;
