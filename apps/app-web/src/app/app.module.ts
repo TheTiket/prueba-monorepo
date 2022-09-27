fd50613e48577f00e14bfa510ce31b15067b3e51
NikolayCarrasco <nikolay.carrasco.v@gmail.com>
2022-09-27 12:36:03 -0300

    chore(app-web/app): save app folder

--- apps/app-web/src/app/app.module.ts
+++ apps/app-web/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule, 
+    BrowserAnimationsModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
