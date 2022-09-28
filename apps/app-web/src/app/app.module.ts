47d8a409b4ca8ac462792c9098803e1ec022d496
Anibal <anibal78e@gmail.com>
2022-09-28 15:04:24 -0300

    feat(libs): models

--- apps/app-web/src/app/app.module.ts
+++ apps/app-web/src/app/app.module.ts
@@ -5,14 +5,15 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { BudgetCardComponent } from './component/budget-card/budget-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BudgetCardComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- libs/models/src/index.ts
+++ libs/models/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/models.module';
+export * from './lib/budget/budget.model';
