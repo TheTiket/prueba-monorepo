c456760d6e090038683ca65e8c61c188fc0a14e4
NikolayCarrasco <71903605+NikolayCarrasco@users.noreply.github.com>
2022-09-29 15:27:10 -0300

    Update app.module.ts

--- apps/app-web/src/app/app.module.ts
+++ apps/app-web/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { BudgetCardComponent } from './component/budget-card/budget-card.component';
 
 @NgModule({
   declarations: [AppComponent, BudgetCardComponent],
