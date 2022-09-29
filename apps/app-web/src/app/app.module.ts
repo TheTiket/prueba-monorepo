24b6abd970fd16169dfbb0374f6cb73bda21c938
Anibal <anibal78e@gmail.com>
2022-09-29 15:33:37 -0300

    fix(app-web): declaration

--- apps/app-web/src/app/app.module.ts
+++ apps/app-web/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, BudgetCardComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
