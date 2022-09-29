a1348969b000cb41362ce36e5637025d02621bc0
Anibal <anibal78e@gmail.com>
2022-09-29 12:44:51 -0300

    style(app-web,app-mobile): date format

--- apps/app-web/src/app/test/components/budget-card/budget-card.component.ts
+++ apps/app-web/src/app/test/components/budget-card/budget-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { Budget } from '@prueba-monorepo/models';
+import * as moment from 'moment';
 @Component({
   selector: 'prueba-monorepo-budget-card',
   templateUrl: './budget-card.component.html',
@@ -7,19 +8,28 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BudgetCardComponent implements OnInit {
 
-  @Input() budget: any;
+  @Input() budget!: Budget;
   public price: string;
-
+  public time: string;
+  public date: string;
   constructor() {
     this.price = '';
+    this.time = '';
+    this.date = '';
   }
 
   ngOnInit(): void {
     this.showPrice();
+    this.showDate();
   }
 
   public showPrice(){
     const newFormat = new Intl.NumberFormat("es-CL", { currency: "CLP", style: "currency" })
     this.price = newFormat.format(this.budget.price);
   }
+  public showDate(){
+    this.date = moment(this.budget.date).subtract(10, 'days').calendar();
+    this.time = moment(this.budget.date).format('LT');
+  }
+
 }
